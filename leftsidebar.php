8060f78384b48996a396c35aeb1d7d99e114867c
Vipin Ashodhiya <ashodhiyavipin@gmail.com>
2015-08-15 11:17:36 +0530

    turned into bootstrap compatible

--- leftsidebar.php
+++ leftsidebar.php
@@ -1,107 +1,85 @@
 <?php
 session_start();
-include("dbconnection.php");
+include ("dbconnection.php");
 ?>
-<section>
-      <address>
-      <?php echo "<b>Welcome ".$fnamesession. " ". $lnamesession ."</b><hr>";
-    
-	if($imgpathsession == "")
-	{	   
-		echo "<img src='images/profilepic.jpg' >";
-	}
-	else
-	{
-		echo "<img src='uploads/$imgpathsession' >";
-	}
-       ?>
-  <br>
-      </address>
-    </section>
-    
-          <aside>
-        <!-- ########################################################################################## -->
- 
-<?php
-$fri = mysqli_query($con,"SELECT * FROM friends where ((profileid1='$_SESSION[profileid]' or profileid2='$_SESSION[profileid]') and requeststatus='accepted') OR ((profileid2='$_SESSION[profileid]' or profileid1='$_SESSION[profileid]') and requeststatus='accepted')");?>
-    <h2>No. of friends - (<?php echo mysqli_num_rows($fri); ?>)</h2>
-     <?php
-            $friend= mysqli_query($con, "SELECT * from friends where ((profileid1='$_SESSION[profileid]' or profileid2='$_SESSION[profileid]') and requeststatus='accepted') OR ((profileid2='$_SESSION[profileid]' or profileid1='$_SESSION[profileid]') and requeststatus='accepted') ");
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<?php echo "<b>Welcome " . $fnamesession . " " . $lnamesession . "</b><hr>";
+			if ($imgpathsession == "") {
+				echo "<img src='images/profilepic.jpg' class='img-thumbnail'>";
+			} else {
+				echo "<img src='uploads/$imgpathsession' class='img-thumbnail'>";
+			}
 			?>
-
-<?php
-while($rs = mysqli_fetch_array($friend))
-{
-	if($rs[profileid1] == $_SESSION[profileid])
-	{
-		$friendsprofileid = $rs[profileid2];
-	}
-	else
-	{
-		$friendsprofileid = $rs[profileid1];
-	}
-	$r=mysqli_query($con, "SELECT * from profile where profileid='$friendsprofileid'");
-	$show= mysqli_fetch_array($r);
-	$img=mysqli_query($con, "SELECT * from images where imgid='$show[imgid]'");
-    $pic= mysqli_fetch_array($img);
-    $profileimage="uploads/".$pic[imagepath];	
-if($pic[imagepath]=="")
-{
-	$profileimage ="images/profilepic.jpg";
-}
-else
-{
-	$profileimage ="uploads/".$pic[imagepath];
-}
-		echo "<a href='viewprofile.php?friend=$friendsprofileid'  class='one_sixth'><img src='$profileimage' class='icon-desktop icon-6x img-rounded'></a>";
-		echo "<p><a href='viewprofile.php?friend=$friendsprofileid'>".$show[firstname]."&nbsp;". $show[lastname]. "</a><hr></p>";	
-
-}
-?>
-
-
-        <!-- /nav -->
-
-        <!-- ########################################################################################## -->
-      </aside>
-    
-      <aside>
-        <!-- ########################################################################################## -->
+  			<br>
+		</div>
+		<div class="col-xs-12"> 
+			<?php
+			$fri = mysqli_query($con, "SELECT * FROM friends where ((profileid1='$_SESSION[profileid]' or profileid2='$_SESSION[profileid]') and requeststatus='accepted') OR ((profileid2='$_SESSION[profileid]' or profileid1='$_SESSION[profileid]') and requeststatus='accepted')");
+			?>
+			    
+				<h2>No. of friends - (<?php echo mysqli_num_rows($fri); ?>)</h2>
+				<?php
+				$friend = mysqli_query($con, "SELECT * from friends where ((profileid1='$_SESSION[profileid]' or profileid2='$_SESSION[profileid]') and requeststatus='accepted') OR ((profileid2='$_SESSION[profileid]' or profileid1='$_SESSION[profileid]') and requeststatus='accepted') ");
+			
+				while ($rs = mysqli_fetch_array($friend)) {
+					if ($rs[profileid1] == $_SESSION[profileid]) {
+						$friendsprofileid = $rs[profileid2];
+					} else {
+						$friendsprofileid = $rs[profileid1];
+					}
+					$r = mysqli_query($con, "SELECT * from profile where profileid='$friendsprofileid'");
+					$show = mysqli_fetch_array($r);
+					$img = mysqli_query($con, "SELECT * from images where imgid='$show[imgid]'");
+					$pic = mysqli_fetch_array($img);
+					$profileimage = "uploads/" . $pic[imagepath];
+					if ($pic[imagepath] == "") {
+						$profileimage = "images/profilepic.jpg";
+					} else {
+						$profileimage = "uploads/" . $pic[imagepath];
+					}
+					echo "<a href='viewprofile.php?friend=$friendsprofileid'  class='one_sixth'><img src='$profileimage' class='icon-desktop icon-6x img-thumbnail'></a>";
+					echo "<p><a href='viewprofile.php?friend=$friendsprofileid'>" . $show[firstname] . "&nbsp;" . $show[lastname] . "</a><hr></p>";
+			
+				}
+			?>
+      	</div>
+      
+      	<div class="col-xs-12">
         <h2>Groups</h2>
         <nav>
- <!-- group #################################################### -->
-   <?php
-	$sql = "SELECT * FROM  groups ORDER BY RAND() ";
-  $qmsg = mysqli_query($con,$sql);
-  $i=0;
-  while($rsmsg = mysqli_fetch_array($qmsg))
-	{   
-				$sql1 = "SELECT * FROM  profile where profileid='$rsmsg[profileid]' ";
- 				$qmsg1 = mysqli_query($con,$sql1);
-  				$rsmsg1 = mysqli_fetch_array($qmsg1);
-
-				$sql2 = "SELECT * FROM  groupmembers where groupid='$rsmsg[groupid]' ";
- 				$qmsg2 = mysqli_query($con,$sql2);
-  				$rsmsg2 = mysqli_fetch_array($qmsg2);	
-				
-				$sql3 = "SELECT * FROM  groupmembers where groupid='$rsmsg[groupid]' and  profileid='$_SESSION[profileid]'";
- 				$qmsg3 = mysqli_query($con,$sql3);
-  				$rsmsg3 = mysqli_fetch_array($qmsg3);
-				
-				if(mysqli_num_rows($qmsg3) == 1)
-				{
-	?>
-					  <p><a href='groupwallpost.php?groupid=<?php echo $rsmsg[groupid] ?>'><strong><?php echo $rsmsg[groupname]; ?></strong></a></p>
+			<?php
+			$sql = "SELECT * FROM  groups ORDER BY RAND() ";
+			$qmsg = mysqli_query($con,$sql);
+			$i=0;
+			while($rsmsg = mysqli_fetch_array($qmsg))
+				{   
+							$sql1 = "SELECT * FROM  profile where profileid='$rsmsg[profileid]' ";
+			 				$qmsg1 = mysqli_query($con,$sql1);
+			  				$rsmsg1 = mysqli_fetch_array($qmsg1);
 			
-
-	<?php
-    			}
-
-    }
-    ?>
-<!---- end group  ##############################################  -->          
+							$sql2 = "SELECT * FROM  groupmembers where groupid='$rsmsg[groupid]' ";
+			 				$qmsg2 = mysqli_query($con,$sql2);
+			  				$rsmsg2 = mysqli_fetch_array($qmsg2);	
+							
+							$sql3 = "SELECT * FROM  groupmembers where groupid='$rsmsg[groupid]' and  profileid='$_SESSION[profileid]'";
+			 				$qmsg3 = mysqli_query($con,$sql3);
+			  				$rsmsg3 = mysqli_fetch_array($qmsg3);
+							
+							if(mysqli_num_rows($qmsg3) == 1)
+							{
+				?>
+								  <p><a href='groupwallpost.php?groupid=<?php echo $rsmsg[groupid] ?>'><strong><?php echo $rsmsg[groupname]; ?></strong></a></p>
+						
+			
+				<?php
+				}
+			
+				}
+			    ?>
         </nav>
-        <!-- /nav -->
-
-        <!-- ########################################################################################## -->
-      </aside>
+      	</div>
+     </div>
+   </div>
+</html>
