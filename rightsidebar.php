f714e855a1f146a5840d5955c7da517737017974
Vipin Ashodhiya <ashodhiyavipin@gmail.com>
2015-09-02 23:49:57 +0530

    fixed the right sidebar events problems

--- rightsidebar.php
+++ rightsidebar.php
@@ -1,37 +1,29 @@
 <?php
-session_start();
-include("dbconnection.php");
-	
-$result= mysqli_query($con,"select * from advertisements where status='Enabled' ORDER BY rand() limit 0,3");
-$resuser = mysqli_query( $con , "SELECT events.*,profile.* from events INNER JOIN profile ON events.profileid=profile.profileid ORDER BY events.eventid desc");
-$wallrs = mysqli_fetch_array($resuser);
-?>
+        $resuser=mysqli_query($con,"SELECT events.*,profile.* from events INNER JOIN profile ON events.profileid=profile.profileid ORDER BY events.eventid desc");
+		echo "<ul class='list-group'>";
+		echo "<li class='list-group-item'>Your upcoming events</li>";
+		while($wallrs = mysqli_fetch_array($resuser))
+		{
+			if($wallrs[1] == $_SESSION[profileid])
+			{
+				$checkwp = 1;
+			}
+			else
+			{
+				$result1 = mysqli_query($con,"SELECT * from friends where (profileid1='$wallrs[profileid]' AND profileid2='$_SESSION[profileid]' and requeststatus='accepted') OR (profileid1='$_SESSION[profileid]' AND profileid2='$wallrs[profileid]' AND requeststatus='accepted')");
+				$checkwp = mysqli_num_rows($result1);
+			}
+				if($checkwp == 1)
+				{
+			?>
+				<li class="list-group-item">
+				<p><strong><?php echo $wallrs[firstname]; ?> <?php echo $wallrs[lastname]; ?> </strong></p>
+				<p><?php echo $wallrs[eventsubject]; ?> </p>
+				<p><?php echo $wallrs[eventdescription]; ?> </p>
+				</li>
+			<?php
+				}
 
-
-
-  <aside class="clear">
-        <!-- ########################################################################################## -->
-        
-        <h2>Upcoming Events in your Family</h2>
-        <section class="clear">
-          <ul class="nospace">
-          <p><?php echo $wallrs[firstname]; ?> <?php echo $wallrs[lastname]; ?></p>
-          <p>has an event on <?php echo $wallrs[eventdate];?> </p>
-          <p>called&nbsp;<?php echo $wallrs[eventsubject]; ?> </p>
-          
-          
-          
-          <?php
-           while($rs=mysqli_fetch_array($result))
-{
-	echo"<li>
-	<strong>$rs[advtname]</strong><br>
-	<a href='$rs[link]' target='_blank'><img src='files/$rs[imagename]' width='200' height='175'/ ></a><hr></li>";
-}
-?>
-          
-          </ul>
-        </section>
-        <!-- /section -->
-        <!-- ########################################################################################## -->
-      </aside>
+		}
+		echo "</ul>";
+		?>
