d21ed8526a32ee7b7f3abfccf5dda807d6f5df8a
Vipin Ashodhiya <ashodhiyavipin@gmail.com>
2015-10-02 16:53:07 +0530

    wallpost external css

--- wallpost.php
+++ wallpost.php
@@ -1,294 +1,13 @@
-<!doctype html>
+<!DOCTYPE html>
 <head>
-	<link href="layout/styles/fonts/font-awesome/font-awesome.min.css" rel="stylesheet">
-	<style>
-	* {
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-}
-
-body {
-  font-family: 'Quicksand';
-  color: #7f8c8d;
-  font-size: 14px;
-  background-color: #ededed; }
-
-.bubble {
-  width: 100%;
-  padding: .5em 1em;
-  line-height: 1.4em;
-  padding: 20px;
-  background-color: #ecf0f1;
-  position: relative;
-  -webkit-border-radius: 8px;
-  -moz-border-radius: 8px;
-  -ms-border-radius: 8px;
-  -o-border-radius: 8px;
-  border-radius: 8px;
-  text-align: left;
-  display: inline-block; }
-  .bubble:hover > .over-bubble {
-    opacity: 1; }
-
-.bubble-container {
-  width: 75%;
-  display: block;
-  position: relative;
-  padding-left: 20px;
-  vertical-align: top;
-  display: inline-block; }
-
-.arrow {
-  content: '';
-  display: block;
-  position: absolute;
-  left: 12px;
-  bottom: 25%;
-  height: 0;
-  width: 0;
-  border-top: 20px solid transparent;
-  border-bottom: 20px solid transparent;
-  border-right: 20px solid #ecf0f1; }
-
-.timeline {
-  width: 560px;
-  display: block;
-  margin: auto;
-  background-color: #dde1e2;
-  padding-bottom: 2em;
-  -webkit-box-shadow: #bdc3c7 0 5px 5px;
-  -moz-box-shadow: #bdc3c7 0 5px 5px;
-  box-shadow: #bdc3c7 0 5px 5px;
-  -moz-border-radius-bottomleft: 8px;
-  -webkit-border-bottom-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-  -moz-border-radius-bottomright: 8px;
-  -webkit-border-bottom-right-radius: 8px;
-  border-bottom-right-radius: 8px;
-  margin-bottom: 2em; }
-  .timeline li {
-    padding: 1em 0; }
-  .timeline li:nth-child(even) {
-    background-color: #d3d7d8; }
-
-.avatar {
-  width: 18%;
-  display: inline-block;
-  vertical-align: top;
-  position: relative;
-  overflow: hidden;
-  margin-left: 2%; }
-  .avatar img {
-    width: 100%;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    -ms-border-radius: 50%;
-    -o-border-radius: 50%;
-    border-radius: 50%;
-    border: 5px solid #ecf0f1;
-    position: relative; }
-  .avatar:hover > .hover {
-    cursor: pointer;
-    opacity: 1; }
-
-.hover {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: #3498db;
-  top: 0;
-  font-size: 1.8em;
-  border: 5px solid #5cc0ff;
-  -webkit-border-radius: 50%;
-  -moz-border-radius: 50%;
-  -ms-border-radius: 50%;
-  -o-border-radius: 50%;
-  border-radius: 50%;
-  text-align: center;
-  color: white;
-  padding-top: 24%;
-  opacity: 0;
-  font-family: 'FontAwesome';
-  font-weight: 300;
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.5s;
-  -moz-transition-duration: 0.5s;
-  -o-transition-duration: 0.5s;
-  transition-duration: 0.5s;
-  -webkit-transition-timing-function: ease;
-  -moz-transition-timing-function: ease;
-  -o-transition-timing-function: ease;
-  transition-timing-function: ease; }
-
-.first {
-  width: 560px;
-  display: block;
-  margin: auto;
-  background-color: #3498db;
-  text-shadow: #2084c7 1px 1px 0;
-  padding: 1em 0 !important;
-  color: white;
-  text-align: center;
-  margin-top: 1em;
-  font-family: "Lato";
-  font-size: 1.6em;
-  -moz-border-radius-topleft: 8px;
-  -webkit-border-top-left-radius: 8px;
-  border-top-left-radius: 8px;
-  -moz-border-radius-topright: 8px;
-  -webkit-border-top-right-radius: 8px;
-  border-top-right-radius: 8px;
-  position: relative; }
-
-.icon-twitter {
-  font-size: 1.5em; }
-
-.new {
-  position: absolute;
-  right: 5%; }
-
-.over-bubble {
-  line-height: 1.4em;
-  padding-top: 10%;
-  background-color: rgba(236, 240, 241, 0.8);
-  position: relative;
-  -webkit-border-radius: 8px;
-  -moz-border-radius: 8px;
-  -ms-border-radius: 8px;
-  -o-border-radius: 8px;
-  border-radius: 8px;
-  text-align: center;
-  display: inline-block;
-  position: absolute !important;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  top: 0;
-  left: 0;
-  z-index: 999;
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.3s;
-  -moz-transition-duration: 0.3s;
-  -o-transition-duration: 0.3s;
-  transition-duration: 0.3s;
-  -webkit-transition-timing-function: ease-in;
-  -moz-transition-timing-function: ease-in;
-  -o-transition-timing-function: ease-in;
-  transition-timing-function: ease-in;
-  font-size: 2.8em;
-  text-shadow: white 1px 1px 0; }
-
-.action {
-  margin-right: .3em;
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.2s;
-  -moz-transition-duration: 0.2s;
-  -o-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-timing-function: ease-in;
-  -moz-transition-timing-function: ease-in;
-  -o-transition-timing-function: ease-in;
-  transition-timing-function: ease-in; }
-
-.icon-star {
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.2s;
-  -moz-transition-duration: 0.2s;
-  -o-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-timing-function: ease;
-  -moz-transition-timing-function: ease;
-  -o-transition-timing-function: ease;
-  transition-timing-function: ease; }
-  .icon-star:hover {
-    cursor: pointer;
-    color: #f39c12; }
-
-.icon-retweet {
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.2s;
-  -moz-transition-duration: 0.2s;
-  -o-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-timing-function: ease;
-  -moz-transition-timing-function: ease;
-  -o-transition-timing-function: ease;
-  transition-timing-function: ease; }
-  .icon-retweet:hover {
-    cursor: pointer;
-    color: #16a085; }
-
-.icon-mail-reply {
-  -webkit-transition-property: all;
-  -moz-transition-property: all;
-  -o-transition-property: all;
-  transition-property: all;
-  -webkit-transition-duration: 0.2s;
-  -moz-transition-duration: 0.2s;
-  -o-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-timing-function: ease;
-  -moz-transition-timing-function: ease;
-  -o-transition-timing-function: ease;
-  transition-timing-function: ease; }
-  .icon-mail-reply:hover {
-    cursor: pointer;
-    color: #3498db; }
-
-h3 {
-  font-size: 1.2em;
-  font-weight: bold;
-  font-family: 'Lato';
-  display: inline-block;
-  margin-bottom: .2em;
-  color: #95a5a6; }
-
-.retweet {
-  position: absolute;
-  opacity: 1;
-  top: 0;
-  right: 1em;
-  display: block;
-  background-color: #16a085;
-  padding: 4px;
-  -moz-border-radius-bottomleft: 5px;
-  -webkit-border-bottom-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  -moz-border-radius-bottomright: 5px;
-  -webkit-border-bottom-right-radius: 5px;
-  border-bottom-right-radius: 5px; }
-  .retweet .icon-retweet {
-    color: white;
-    margin: auto;
-    width: 100%;
-    display: block;
-    font-size: 1.2em; }
-
-
-	</style>
+<link rel="stylesheet" type="text/css" href="layout/styles/wallpost.css">
 </head>
+<!-- content -->
 <?php
 session_start();
 include("fancybox.php");
 include("wallpostcodings.php");
 ?>
-<!-- content -->
 <body>
   <div class="container-fluid">
     <!-- ################################################################################################ -->
@@ -298,66 +17,51 @@ include("wallpostcodings.php");
     </div>
     <!-- ################################################################################################ -->
     <div class="col-xs-12 col-sm-8 col-md-8 col-lg-7">
-        <h2>Wallpost</h2>
+      <h2>Wallpost</h2>
         <div class="tab-wrapper clear">
-          <ul class="tab-nav clear">
+          <ul class="nav nav-pills clear">
             <li><a href="#tab-1">Wall</a></li>
             <li><a href="#tab-2">Photos</a></li>
             <li><a href="#tab-3">Videos</a></li>
           </ul>
-          <div class="tab-container">
-            		<!-- Upload message -->
-            		<div id="tab-1" class="tab-content clear">
-            		        <form class="rnd5" action="" method="post" onsubmit="return validate()" name="messageform">
-                            <input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">
-                          	
-                            <div class="form-message">
-            				<textarea name="message" id="message" cols="25" rows="3"></textarea>
-          					</div>
-          					<p>
-            				<input type="submit" value="Submit" name="submitwall">
-            				&nbsp;
-          					</p>
-       						</form>
-            		</div>
-                    <!-- Upload Photo -->
-                    <div id="tab-2" class="tab-content clear">
-                            <form id="myUploadForm" name="myUploadForm" action="" method="post" enctype="multipart/form-data" onsubmit="return validate1()">
-                            <input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">                            
-                              <div class="form-message">
-                              <div id="uploadajax">
-                              <input type="file" name="uploadphotos[]" id="uploadphotos[]" multiple="multiple"  accept="image/*"/>
-                              	<ul id="filelist">
-                                
-								</ul>
-                              </div>
-                             <textarea name="message" id="message" cols="25" rows="2"></textarea>
-                              </div>
-                              <p>
-                                <input type="submit" value="Submit" name="submitphotos" id="submitphotos">
-                                &nbsp;
-                              </p>
-                            </form>
-            		</div>
-                    
-                    <!-- Upload video -->
-                    <div id="tab-3" class="tab-content clear">
-                            <form class="rnd5" name="uploadvideos" action="" method="post" enctype="multipart/form-data"  onsubmit="return validate2()">
-                    		<input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">
-                              <div class="form-message">
-                               <input name="uploadvideo" id="uploadvideo" type="file"  accept="video/mp4" />
-                                <textarea name="message" id="message" cols="25" rows="2"></textarea>
-                              </div>
-                              <p>
-                                <input type="submit" value="Submit" name="submitvideos">
-                                &nbsp;
-                              </p>
-                            </form>
-                            
-            		</div>
-            		<!-- / Tab Content -->
-          		</div>
-        </div>
+          <div>
+            <!-- Upload message -->
+            <div id="tab-1" class="tab-content clear">
+              <form class="rnd5" action="" method="post" onsubmit="return validate()" name="messageform">
+                <input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">
+                <div class="form-message">
+                  <textarea name="message" id="message" cols="25" rows="3" placeholder="Your status update goes here..."></textarea>
+                </div>
+                <p><input type="submit" value="Submit" name="submitwall">&nbsp;</p>
+              </form>
+            </div>
+            <!-- Upload Photo -->
+            <div id="tab-2" class="tab-content clear">
+            <form id="myUploadForm" name="myUploadForm" action="" method="post" enctype="multipart/form-data" onsubmit="return validate1()">
+              <input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">
+                <div class="form-message">
+                  <div id="uploadajax">
+                  <input type="file" name="uploadphotos[]" id="uploadphotos[]" multiple="multiple"  accept="image/*"/>
+                  <ul id="filelist"></ul>
+                  </div>
+                  <textarea name="message" id="message" cols="25" rows="2" placeholder="Your status update goes here..."></textarea>
+                </div>
+                <p><input type="submit" value="Submit" name="submitphotos" id="submitphotos">&nbsp;</p>
+            </form>
+            </div>
+            <!-- Upload video -->
+            <div id="tab-3" class="tab-content clear">
+              <form class="rnd5" name="uploadvideos" action="" method="post" enctype="multipart/form-data"  onsubmit="return validate2()">
+                <input type="hidden" name="setid" value="<?php echo $_SESSION[setid]; ?>">
+                  <div class="form-message">
+                    <input name="uploadvideo" id="uploadvideo" type="file"  accept="video/mp4" />
+                    <textarea name="message" id="message" cols="25" rows="2" placeholder="Your status update goes here..."></textarea>
+                  </div>
+                  <p><input type="submit" value="Submit" name="submitvideos">&nbsp;</p>
+              </form>
+            </div>
+          </div>
+      </div>
 <br />
 <!-- WALL POST Message ################################3 -->
 <?php
@@ -627,18 +331,29 @@ echo "<a href='wallpost.php?delcmntid=$rs1[0]'><strong>Delete</strong></a> </p>"
 $cmtlikesstatus=0;
 //Likes Comments code ends here		
 echo "</div>";
-		}
+		} ?>
+
+<div class='comment-container' >
+          <form class="rnd5" action="" method="post">
+                    <input type="hidden" name="comsession" value="<?php echo $_SESSION[setcommentid]; ?>" />
+                    <input type="hidden" name="publishid" value="<?php echo $rs[0]; ?>" />             
+                    <textarea name="commentmessage" id="commentmessage" cols="25" rows="4" placeholder="Type your comments here..."></textarea>
+                    <input type="submit" value="Submit" name="submitcomment">
+        </form>
+</div>
+
+<?php 
 echo " </li> </ul>";
 ?>
 <!-- Comment post with toggle box starts here  -->
-<div class='comments info' >
+<!-- <div class='comments info' >
 		      <form class="rnd5" action="" method="post">
                   	<input type="hidden" name="comsession" value="<?php echo $_SESSION[setcommentid]; ?>" />
                   	<input type="hidden" name="publishid" value="<?php echo $rs[0]; ?>" />             
                   	<textarea name="commentmessage" id="commentmessage" cols="25" rows="2"></textarea>
                     <input type="submit" value="Submit" name="submitcomment">
 				</form>
-</div>
+</div> -->
 <!-- Comment post with toggle box ends here  -->  
 <?php
 	}
