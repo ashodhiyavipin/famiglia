c97c264685acae59d4f771639598ae132842f218
Vipin Ashodhiya <ashodhiyavipin@gmail.com>
2021-02-12 17:34:19 +0530

    Database file updated for local development.

--- dbconnection.php
+++ dbconnection.php
@@ -1,11 +1,11 @@
 <?php
 // Mysql_connect function to connect database
 // localhost is server name(host name) , root is username, technology is password and social networking is datbase name
-$con=mysqli_connect("sql201.byethost8.com","b8_16401901","123qwer","b8_16401901_socialnetwork");
+$con=mysqli_connect("localhost","root","","socialnetwork");
 	// Check connection is connected or not
 	if (mysqli_connect_errno())
 	  {
-		  //This statement executes if any error in connection 
+		  //This statement executes if any error in connection
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	  }
   ?>
